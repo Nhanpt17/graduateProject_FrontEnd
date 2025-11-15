02862aa44fa3cd2180ae8b07e911ccd89cc669fd
Nhanpt17 <113848737+Nhanpt17@users.noreply.github.com>
2025-11-15 17:04:29 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
     production: true,
-    BASIC_URL: "https://graduateproject.onrender.com/"
+    BASIC_URL: "https://graduateproject.onrender.com/",
+    PAYPAL_CLIENT_ID: "AeXAQ2FDcjyWLHclWO1K1znQpemDqPa96Xb0v5s4uOJwabIbzLyAxLEfYVot_teQJfj-kwpSCNyklDmu",
+    VNPAY_SECRET_KEY:"TVA44OJ17MFUDUV14POP52TY193OKKAY",
+    goongApiKey: 'QDO8jP3oYIwGQyOtHG0C0pqbT3UW3tFkr6aZkfp2',
+    goongMapKey: 'l3am4RwPIX9jsM0CT4hQLpx6CthSkWXlfG54e9IX'
   };
   
