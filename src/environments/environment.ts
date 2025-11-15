b66cf9ce07cbbe70483eaa65b9f63f1c3f0b2d1f
Nhanpt17 <113848737+Nhanpt17@users.noreply.github.com>
2025-11-15 17:04:58 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment ={
     production: false,
-    BASIC_URL:"http://localhost:8080/"
+    BASIC_URL:"http://localhost:8080/",
+    PAYPAL_CLIENT_ID: "AeXAQ2FDcjyWLHclWO1K1znQpemDqPa96Xb0v5s4uOJwabIbzLyAxLEfYVot_teQJfj-kwpSCNyklDmu",
+    VNPAY_SECRET_KEY:"TVA44OJ17MFUDUV14POP52TY193OKKAY",
+    goongApiKey: 'QDO8jP3oYIwGQyOtHG0C0pqbT3UW3tFkr6aZkfp2',
+    goongMapKey: 'l3am4RwPIX9jsM0CT4hQLpx6CthSkWXlfG54e9IX'
 };
